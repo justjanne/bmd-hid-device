fa10c1e9069020302085821b2e2514d8e0e7fbc8
Janne Mareike Koschinski <janne@kuschku.de>
2023-05-04 19:59:22 +0200

    fix: correct dependencies

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ name = "bmd-hid-device"
 description = "library to provide access to blackmagicdesign hid devices"
 
 setup(name=name,
-      version="0.1.4",
+      version="0.1.5",
       description=description,
       url="https://github.com/justjanne/bmd-hid-device",
       project_urls={
           "GitHub": "https://github.com/justjanne/bmd-hid-device",
           "Issue tracker": "https://github.com/justjanne/bmd-hid-device/issues",
       },
-      install_requires=[],
+      install_requires=["hid"],
       packages=['bmd_hid_device'],
       package_data={'bmd_hid_device': ['py.typed']},
       license="Apache-2.0 license",
